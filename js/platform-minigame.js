b1077d884bdfdcd5f6a1b8b279df1c468711eec7
wangzhan <wangzhan@shizhuang-inc.com>
2026-08-26 10:56:58 +0800

    Remove surface tile side borders between repeats

--- js/platform-minigame.js
+++ js/platform-minigame.js
@@ -2319,7 +2319,7 @@
   function drawFloor(L) {
     if (sceneSprites.allLoaded) {
       drawTile(sceneSprites.brickBrown, 0, L.pathY, W, H - L.pathY, { fit: "tile", step: 36 });
-      drawTile(sceneSprites.surface, L.pathX, L.pathY, L.pathW, 36, { fit: "tile", step: 36 });
+      drawSurfaceStrip(L.pathX, L.pathY, L.pathW, 36);
     } else {
       ctx.fillStyle = "#4a525a";
       ctx.fillRect(0, L.pathY, W, H - L.pathY);
@@ -2487,7 +2487,7 @@
       ctx.fillRect(0, L.pathY, W, H - L.pathY);
     }
     if (sceneSprites.allLoaded) {
-      drawTile(sceneSprites.surface, L.pathX, L.pathY, L.pathW, 36, { fit: "tile", step: 36 });
+      drawSurfaceStrip(L.pathX, L.pathY, L.pathW, 36);
     } else {
       ctx.fillStyle = "#8d9196";
       ctx.fillRect(L.pathX, L.pathY, L.pathW, L.pathH);
@@ -2627,6 +2627,25 @@
       return;
     }
     ctx.drawImage(img, 0, 0, sw, sh, x, y, w, h);
+  }
+
+  // The surface sprite has a 2px framing border on both sides. Crop it out
+  // while keeping the top edge, so repeated grass tiles touch without gaps.
+  function drawSurfaceStrip(x, y, w, h) {
+    var img = sceneSprites.surface;
+    var sw = img.naturalWidth || 18;
+    var sh = img.naturalHeight || 18;
+    var sourceX = Math.min(2, Math.max(0, sw - 1));
+    var sourceW = Math.max(1, sw - sourceX * 2);
+    var step = 36;
+    x = Math.round(x);
+    y = Math.round(y);
+    w = Math.round(w);
+    h = Math.round(h);
+    for (var tx = x; tx < x + w; tx += step) {
+      var rw = Math.min(step, x + w - tx);
+      ctx.drawImage(img, sourceX, 0, sourceW, sh, tx, y, rw, h);
+    }
   }
 
   function draw() {
